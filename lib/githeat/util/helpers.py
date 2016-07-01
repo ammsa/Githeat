c4f93c8f2c776acc86987c9f931ddb9ef59e7fa4
Mustafa Abualsaud <ammsa@users.noreply.github.com>
2016-07-01 15:21:18 -0400

    Add method to normalize list of tuples

--- lib/githeat/util/helpers.py
+++ lib/githeat/util/helpers.py
@@ -7,9 +7,10 @@ import calendar
 import math
 
 import datetime
+from operator import itemgetter
 
 
-def normalize(dictionary, x, y):
+def normalize_dict(dictionary, x, y):
     """
     Normalize values in dictinoary to be in range [x, y]
 
@@ -18,19 +19,57 @@ def normalize(dictionary, x, y):
     :param y: range max
     :return: dict with values changed accordingly
     """
-    #  normalize to [0, 1]
     min_value = min(dictionary.values())
     max_value = max(dictionary.values())
+    if max_value <= y and min_value >= x:
+        return dictionary
+
+    #  normalize to [0, 1]
     range1 = max_value - min_value
+    if not range1:
+        range1 = 1
     for key in dictionary:
-        dictionary[key] = (dictionary[key] - min_value) / range1
+        dictionary[key] = (float(dictionary[key]) - min_value) / range1
 
     # then scale [x,y] and take ceiling
     range2 = y - x
     for key in dictionary:
-        dictionary[key] = math.ceil((dictionary[key] * range2) + x)
+        dictionary[key] = math.ceil((float(dictionary[key]) * range2) + x)
 
     return dictionary
+
+
+def normalize_tuple_list(tuple_list, x, y):
+    """
+    Normalize values in a list of tuples to be in range [x, y]
+
+    :param tuple_list:
+    :param x: range min
+    :param y: range max
+    :return: list of tuples with values changed accordingly
+    """
+    min_value = min(tuple_list, key=itemgetter(1))[1]
+    max_value = max(tuple_list, key=itemgetter(1))[1]
+    if max_value <= y and min_value >= x:
+        return tuple_list
+
+    #  normalize to [0, 1]
+    range1 = max_value - min_value
+    if not range1:
+        range1 = 1
+    for idx, pair in enumerate(tuple_list):
+        key = pair[0]
+        value = (float(pair[1]) - min_value) / range1
+        tuple_list[idx] = (key, value)
+
+    # then scale [x,y] and take ceiling
+    range2 = y - x
+    for idx, pair in enumerate(tuple_list):
+        key = pair[0]
+        value = math.ceil((float(pair[1]) * range2) + x)
+        tuple_list[idx] = (key, value)
+
+    return tuple_list
 
 
 def get_months(start_date, months, include_year=False):
